c4e0ef492fd25130c1141d94a6fab7c4b94c6382
AMM9394 <15827061459@163.com>
2019-01-30 15:46:48 +0800

    websorket

--- testJs/num1_twoSum/myTest1.js
+++ testJs/num1_twoSum/myTest1.js
@@ -5,18 +5,30 @@
  */
 var twoSum = function (nums, target) {
   let result = [];
+  for (let k1 in nums){
+    let index2 = nums.findIndex((i2)=>{
+      return nums[k1]+i2 === target;
+    });
+    if (index2) {
+      result = [k1,index2];
+      break;
+    }
+  }
+  /*
   nums.map((i1, k1) => {
-    nums.map((i2,k2)=>{
+    /!*nums.map((i2,k2)=>{
       if (k2>k1) {
         if (i1+i2===target)
           result = [k1,k2];
       }
-    })
-  });
+    })*!/
+
+
+  });*/
   return result;
 };
 
-const nums =[2, 11, 15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,7];
+const nums =[2, 7,11, 15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,7];
 const target = 9;
 console.time('time');
 const result = twoSum(nums,target);
